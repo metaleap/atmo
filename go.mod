6cba5814241f0631353c422214688ab45980374b
Phil Schumann <metaleap@users.noreply.github.com>
2024-07-05 08:18:12 +0200

    polyglot / atmo tweaks

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/metaleap/atmo
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	_ "github.com/metaleap/polyglot-lsp/lang_go/lsp_v3.17"
+)
+
+func main() {
+	println("FOO")
+}
