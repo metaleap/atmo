6ac56b966943a92b4e5a28b0320fe8816aa6e939
Phil Schumann <metaleap@users.noreply.github.com>
2024-07-05 08:49:50 +0200

    polyglot / atmo tweaks

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/metaleap/atmo
 
 go 1.22.4
+
+require github.com/metaleap/polyglot-lsp/lang_go/lsp_v3.17 v0.0.0-20240705061817-51b209e3cef6
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	_ "github.com/metaleap/polyglot-lsp/lang_go/lsp_v3.17"
+	lsp "github.com/metaleap/polyglot-lsp/lang_go/lsp_v3.17"
 )
 
 func main() {
-	println("FOO")
+	var lsp_server lsp.Server
+
+	lsp_server.LogPrefixSendRecvJsons = "atom"
+
+	lsp_server.On_textDocument_hover = func(params *lsp.HoverParams) (any, error) {
+		str := lsp.String("Test Hover")
+		return &lsp.Hover{
+			Contents: lsp.MarkupContentOrMarkedStringOrMarkedStrings{MarkedString: lsp.MarkedString{String: &str}},
+		}, nil
+	}
+
+	panic(lsp_server.Forever())
 }
