3ad8cb9eeb009e6e8a54b67a653bd5eb9730b8a5
Phil Schumann <metaleap@users.noreply.github.com>
2024-07-05 15:10:39 +0200

    polyglot / atmo tweaks

--- main.go
+++ main.go
@@ -10,10 +10,16 @@ func main() {
 	lsp_server.LogPrefixSendRecvJsons = "atom"
 
 	lsp_server.On_textDocument_hover = func(params *lsp.HoverParams) (any, error) {
-		str := lsp.String("Test Hover")
+		str := lsp.String("**Test** _Hover_")
 		return &lsp.Hover{
 			Contents: &lsp.MarkupContentOrMarkedStringOrMarkedStrings{MarkedString: &lsp.StringOrLanguageStringWithValueString{String: &str}},
 		}, nil
+	}
+
+	lsp_server.On_workspace_executeCommand = func(params *lsp.ExecuteCommandParams) (any, error) {
+		if params.Command == "announce-atmo-vscode-ext" {
+		}
+		return nil, nil
 	}
 
 	panic(lsp_server.Forever())
